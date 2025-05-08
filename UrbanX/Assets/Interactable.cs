dc502b85ca8f729d73d178325a80e76cd58dde0a
zsj <Jack_zhang.sj@outlook.com>
2025-05-08 16:52:32 -0700

    interact

---
+++ UrbanX/Assets/Interactable.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject interactIcon;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

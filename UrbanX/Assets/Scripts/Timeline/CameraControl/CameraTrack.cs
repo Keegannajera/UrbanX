1f30d274b40e8560c980f004efdf904e4c9257e5
Allan Tsai <wltsai@uci.edu>
2025-05-20 16:51:52 -0700

    Cut scene - Camera movement

---
+++ UrbanX/Assets/Scripts/Timeline/CameraControl/CameraTrack.cs
@@ -0,0 +1,10 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+[TrackColor(0,1,0)]
+[TrackClipType(typeof(CameraClip))]
+[TrackBindingType(typeof(Camera))]
+public class CameraTrack : TrackAsset
+{
+}

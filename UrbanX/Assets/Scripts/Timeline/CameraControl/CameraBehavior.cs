03909d353ae00b26cdf5cd8f71f7068f2f3c6ef6
Aoli3667 <148655828+Aoli3667@users.noreply.github.com>
2025-05-27 16:57:45 -0700

    Test

--- UrbanX/Assets/Scripts/Timeline/CameraControl/CameraBehavior.cs
+++ UrbanX/Assets/Scripts/Timeline/CameraControl/CameraBehavior.cs
@@ -1,16 +1,63 @@
+using System;
+using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
+using UnityEngine.Playables;
 
-public class CameraBehavior : MonoBehaviour
+[Serializable]
+public class CameraBehavior : PlayableBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private bool m_Change_Pos;
+    [SerializeField] private Vector3 targetPos;
+    [SerializeField] private bool m_Change_Rotation;
+    [SerializeField] private Quaternion m_Rotation;
+    [SerializeField] private float m_Size;
+    private Vector3 originalPos;
+    private Quaternion originalRotation;
+    private Vector3 ogPos = Vector3.negativeInfinity;
+    private Camera controllingCam;
+
+    
+
+
+
+    public override void ProcessFrame(Playable playable, FrameData info, object playerData)
     {
+        controllingCam = playerData as Camera;
+        if (controllingCam == null)
+            return;
         
+        float normalizedTime = (float)(playable.GetTime() / playable.GetDuration());
+
+        if(m_Change_Pos)
+            controllingCam.transform.position = Vector3.Lerp(originalPos, targetPos, normalizedTime);
+        if(m_Change_Rotation)
+            controllingCam.transform.rotation = Quaternion.Lerp(originalRotation, m_Rotation, normalizedTime);
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnBehaviourPlay(Playable playable, FrameData info)
     {
-        
+        controllingCam = info.output.GetUserData() as Camera;
+        originalPos = controllingCam.transform.position;
+        originalRotation = controllingCam.transform.rotation;
+    }
+
+    public override void OnPlayableDestroy(Playable playable)
+    {
+        if (controllingCam == null)
+            return;
+
+        //USeless
+        //controllingCam.transform.rotation = originalRotation;
+        //controllingCam.transform.position = originalPos;
+        //ogPos = Vector3.negativeInfinity;
+    }
+
+    public override void OnBehaviourPause(Playable playable, FrameData info)
+    {
+        if (controllingCam == null)
+            return;
+        //controllingCam.transform.rotation = originalRotation;
+        //controllingCam.transform.position = originalPos;
+        //ogPos = Vector3.negativeInfinity;
     }
 }
